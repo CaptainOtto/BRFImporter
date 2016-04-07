68e7e41a3c2a875d343b846fed7a5dbe2c63e26b
MurbruksProdukt <johan.ganeteg@gmail.com>
2016-04-07 11:47:48 +0200

    ok go

--- Fetch.cpp
+++ Fetch.cpp
@@ -1,5 +1,22 @@
 #include "Fetch.h"
 
+
+void BRFImporter::Fetch::LoadFile(std::string fileName)
+{
+	//read file
+	// read meshdata
+	//fill structs for meshdata with data
+	// create isntance of right subclass (like meshdata); call meshdata constructor; fill meshdatas structs weith data; (skicka in structarna)
+	// remove the temp structs;
+	
+	
+
+}
+
+
+
+
+
 //CON
 BRFImporter::Fetch::Fetch(std::string fileName)
 {
--- Fetch.h
+++ Fetch.h
@@ -19,6 +19,8 @@ namespace BRFImporter
 		void MorphAnimData();
 		void AnimationData();
 
+		void LoadFile(std::string fileName);
+
 		Fetch(std::string fileName);
 		Fetch();
 		~Fetch();
