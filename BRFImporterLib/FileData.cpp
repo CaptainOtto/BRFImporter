b05c701bf5a0ba4fa6d77f65379960892fb0e717
MurbruksProdukt <johan.ganeteg@gmail.com>
2016-04-08 13:14:56 +0200

    goign strong

---
+++ BRFImporterLib/FileData.cpp
@@ -0,0 +1,16 @@
+#include "FileData.h"
+using namespace BRFImporterLib;
+//FUNCTION DEFINITIONS FOR MESHDATA
+void FileData::LoadFile()
+{
+
+}
+FileData::FileData()
+{
+
+}
+
+FileData::~FileData()
+{
+
+}
--- BRFImporterLib/FileData.h
+++ BRFImporterLib/FileData.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <string>
+#include "BRFImporterStructs.h"
+
+namespace BRFImporterLib
+{
+	class FileData
+	{
+	protected:
+
+	public:
+		void LoadFile(std::string fileName);
+
+		//CON DECON
+		FileData();
+		~FileData();
+	};
+}
