7509557610f6ff244cae9b6e7c87da5bc58c9f1f
MurbruksProdukt <johan.ganeteg@gmail.com>
2016-04-13 13:54:41 +0200

    quick organizing

--- BRFImporterLib/FileData.h
+++ BRFImporterLib/FileData.h
@@ -10,17 +10,17 @@ namespace BRFImporterLib
 	class FileData
 	{
 	protected:
-		
-		void LoadMain(std::ifstream *inFile);
-		void LoadMesh(std::ifstream *inFile);
-		void LoadLight(std::ifstream *inFile);
-
 		//MeshBlock
 		MainHeader mainStruct;
 		MeshData thisMesh;
 		MeshHeader* meshStruct;
 		VertexHeader* vertices;
 		IndexHeader* indices;
+
+		//funcBlock
+		void LoadMain(std::ifstream *inFile);
+		void LoadMesh(std::ifstream *inFile);
+		void LoadLight(std::ifstream *inFile);
 	public:
 		Fetch fetch;
 		//MORE BOOLS, -50 DKP!
