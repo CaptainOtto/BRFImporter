d8e4829203ae6583aa5847872b836707ed755bec
Kaptenotto <johancarlphilipottosson@gmail.com>
2016-05-23 15:01:34 +0200

    commit

--- BRFImporterLib/LightData.h
+++ BRFImporterLib/LightData.h
@@ -4,20 +4,36 @@
 
 namespace BRFImporterLib
 {
+
+	class LightContainer
+	{
+	public:
+		std::shared_ptr<LightHeader> lightData;
+		std::unique_ptr<SpotLightHeader[]> spotLightData;
+		std::unique_ptr<AreaLightHeader[]> areaLightData;
+		std::unique_ptr<PointLightHeader[]> pointLightData;
+		std::unique_ptr<DirLightHeader[]> dirLightData;
+		LightContainer(
+			unsigned int spotCount,
+			unsigned int areaCount,
+			unsigned int pointCount,
+			unsigned int dirCount
+		);
+		~LightContainer();
+	};
+
 	class LightData
 	{
 	protected:
-		LightHeader lightData;
-		SpotLightHeader* spotLightData;
-		AreaLightHeader* areaLightData;
-		PointLightHeader* pointLightData;
-		DirLightHeader* dirLightData;
+		std::shared_ptr<LightContainer> lightDataContainer;
 	public:
-		LightHeader GetLightData();
-		SpotLightHeader* GetSpotLightData();
-		AreaLightHeader* GetAreaLightData();
-		PointLightHeader* GetPointLightData();
-		DirLightHeader* GetDirLightData();
+		void SetData(std::shared_ptr<LightContainer> SrcMeshData);
+
+		LightHeader* GetLightData();
+		SpotLightHeader GetSpotLightData(unsigned int iD);
+		AreaLightHeader GetAreaLightData(unsigned int iD);
+		PointLightHeader GetPointLightData(unsigned int iD);
+		DirLightHeader GetDirLightData(unsigned int iD);
 
 		//CON DECON
 		LightData();
